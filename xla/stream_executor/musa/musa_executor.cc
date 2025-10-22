a9ef01b1bdd3d1dc234da68e963c18e08569d067
LMW <1512700996@qq.com>
2025-10-22 11:21:24 +0800

    enable AsBlas

--- xla/stream_executor/musa/musa_executor.cc
+++ xla/stream_executor/musa/musa_executor.cc
@@ -1160,7 +1160,23 @@ void MusaExecutor::DeallocateStream(Stream* stream) {
 }
 
 blas::BlasSupport* MusaExecutor::AsBlas() {
-  return nullptr;
+  absl::MutexLock lock(&mu_);
+  if (blas_ != nullptr) {
+    return blas_.get();
+  }
+
+  PluginRegistry* registry = PluginRegistry::Instance();
+  absl::StatusOr<PluginRegistry::BlasFactory> status =
+      registry->GetFactory<PluginRegistry::BlasFactory>(musa::kMUSaPlatformId);
+  if (!status.ok()) {
+    LOG(ERROR) << "Unable to retrieve BLAS factory: "
+               << status.status().message();
+    return nullptr;
+  }
+
+  auto blas = status.value()(this);
+  blas_.reset(blas);
+  return blas_.get();
 }
 
 dnn::DnnSupport* MusaExecutor::AsDnn() {
